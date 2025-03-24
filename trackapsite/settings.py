354743f5de78e7c7bda0f8ba461e9a0045429c72
joshua <joshuartiongco@gmail.com>
2025-03-24 09:39:23 +0800

    static only

--- trackapsite/settings.py
+++ trackapsite/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # ]
 
 STATIC_URL = 'static/'
-STATIC_ROOT='/home/trackaps/trackapsite/trackapsite/static'
+STATIC_ROOT='static'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'static')
