b4bcbb4ccf8fe91f967e5bfae9638121cb54ffdd
joshua <joshuartiongco@gmail.com>
2025-03-25 13:31:12 +0800

    last static

--- trackapsite/settings.py
+++ trackapsite/settings.py
@@ -139,12 +139,12 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 # STATIC_ROOT='/home/trackaps/trackapsite/static'
 
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn","static_root")
-
 STATIC_URL = '/static/'
 
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn","static_root")
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = '/home/trackaps/trackapsite/trackapsite/static'
+STATIC_ROOT='/home/trackaps/trackapsite/trackapsite/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
