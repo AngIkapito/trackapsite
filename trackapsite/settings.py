cc900d58f5c676b869963858d38e2493c3396fe8
joshua <joshuartiongco@gmail.com>
2025-03-25 13:25:07 +0800

    something

--- trackapsite/settings.py
+++ trackapsite/settings.py
@@ -139,14 +139,15 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 # STATIC_ROOT='/home/trackaps/trackapsite/static'
 
-STATIC_URL = '/static/'
-
 # STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn","static_root")
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/trackaps/trackapsite/trackapsite/static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'staticfiles')
+    os.path.join(BASE_DIR,'static')
 ]
 
 # Default primary key field type
