14fbe55f05a24bc96c703ec1ea3917c7f1ab60e4
joshua <joshuartiongco@gmail.com>
2025-03-25 18:08:35 +0800

    added staticfiles urlpatterns

--- trackapsite/urls.py
+++ trackapsite/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .import views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
@@ -27,3 +29,6 @@ urlpatterns = [
     path('', views.HOME, name='home'),
     
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+
